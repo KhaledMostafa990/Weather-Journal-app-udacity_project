1bac8598d2061f2cbf5a1b419b326cfbc3e1040e
khaledfarghly <khaledfarghly9991@gmail.com>
2021-08-28 11:35:03 +0200

    set a varibles

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = { greating: 'Hello there' };
 
 // Require Express to run server and routes
-import { static } from "express";
+const express = require('express');
+
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
-app.use(static("website"));
+app.use(express.static('website'));
 
 // Setup Server
 const port = 8080;
+
 // Spin up the server
 const server = app.listen(port, () => {
-  console.log("server is running");
+  // console.log('server is running');
   console.log(`running on localhost: ${port}`);
 });
 
 // GET route
 
+app.get('/all', (req, res) => {
+  res.send(projectData);
+});
+
 // POST route
+app.post('/add', (req, res) => {
+  res.send(projectData);
+});
+
+// node server.js
--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
 /* Global Variables */
+const apiKey = '416853b767e9d161d6666279c80bc9cd';
+
+let zipCode = document.querySelector('#zip');
+let feelings = document.querySelector('#feelings');
+let generateBtn = document.querySelector('#generate');
+let date = document.querySelector('#date');
+let temp = document.querySelector('#temp');
+let content = document.querySelector('#content');
+let entryHolder = document.querySelector('#entryHolder');
+
+let url = `https://api.openwethermap.org/data/2.5/weather?zip=&appid=`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
